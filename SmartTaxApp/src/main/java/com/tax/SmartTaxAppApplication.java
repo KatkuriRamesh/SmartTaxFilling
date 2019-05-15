5a27f1c59b1c20cb1850235af01b31b65e2d0929
Ramesh <katkuri.ramesh@impressico.com>
2019-05-15 19:46:24 +0530

    intial commit

---
+++ SmartTaxApp/src/main/java/com/tax/SmartTaxAppApplication.java
@@ -0,0 +1,13 @@
+package com.tax;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartTaxAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartTaxAppApplication.class, args);
+	}
+
+}
